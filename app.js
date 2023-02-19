b9b94b6d61d1a33f837ff1a51450b52e02f7885e
Ons <ons.diweni@esprit.tn>
2023-02-20 00:01:25 +0100

    updating

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var mongoose = require('mongoose')
+var dbConfig = require('./dbConfig.json')
 
 //La méthode express() crée une nouvelle instance d'application Express
 //l'instance app peut être considérée comme une pile de fonctions middleware qui peuvent intercepter et traiter les requêtes HTTP 
@@ -17,7 +18,17 @@ var logger = require('morgan');
 var app = express();
 
 
-//
+//DataBase configuration 
+//se connecter à une base de données MongoDB hébergée dans le cloud sur la plate-forme MongoDB Atlas.
+mongoose.connect(dbConfig.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+//Cors configuration 
 app.use((req, res, next) => {
     //permettra à toutes les demandes de toutes les origines d'accéder à votre API
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -30,20 +41,25 @@ app.use((req, res, next) => {
 
 //these application-level middelwares with no mount path : intercept any request since there is no path is defined.
 app.use(logger('dev'));
+//Avec ceci, Express prend toutes les requêtes qui ont comme Content-Type  application/json 
+// et met à disposition leur  body  directement sur l'objet req
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-//Configuration des routes .
+//Configuration des routes :
 //charge le module ./routes/index.js qui peut contenir un ensemble de routes 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-//app.use([path,] callback) est une méthode Express utilisée pour spécifier une fonction middleware qui sera exécutée pour toutes les requêtes entrantes avant qu 'elles n'atteignent leur route correspondante.
+
+//app.use([path,] callback) est une méthode Express utilisée pour spécifier une fonction middleware 
+//qui sera exécutée pour toutes les requêtes entrantes avant qu'elles n'atteignent leur route correspondante.
 //path est une chaîne de caractères optionnelle qui représente le chemin d'accès sur lequel le middleware doit être exécuté. 
 //Si aucune valeur n'est fournie pour path, le middleware sera exécuté pour toutes les requêtes entrantes.
+
 //Dans cette ligne elle est utilisé pour enregistrer les routes définies dans indexRouter comme routes de niveau supérieur pour l'application
 app.use('/', indexRouter);
 //intercept requests with path begin with "/users"
@@ -53,10 +69,18 @@ app.use('/users', usersRouter);
 
 
 
-app.use('/staff', (req, res, next) => {
+/* app.use('/staff', (req, res, next) => {
     const staff = [{ nom: 'Ons', prenom: 'Diweni', age: 25 }, { nom: 'Houssem', prenom: 'GHallabi', age: 35 }]
     res.json(staff)
 
+}) */
+
+
+app.post('/add', (req, res, next) => {
+    console.log(req.body)
+    res.status(201).json({
+        message: 'Objet crèe avec sucèes '
+    })
 })
 
 
